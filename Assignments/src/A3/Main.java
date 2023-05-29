d21f93cb0e5274e0c7d759bbb0267a8b59da941b
mellisa-phongsa <99854482+mellisa-phongsa@users.noreply.github.com>
2023-05-28 21:48:55 -0600

    fixed main

--- Assignments/src/A3/Main.java
+++ Assignments/src/A3/Main.java
@@ -25,12 +25,7 @@ public class Main {
                 //print number tree created
                 System.out.println("\nTree #" + numTree);
 
-                //create variables
-                Node<Integer> root = t.getRoot();
-                int heightbst = t.height();
-                boolean perfect = t.isPerfect();
-                boolean complete = t.isComplete();
-                boolean doubles = t.hasDoubles();
+                
 
                 //generate a tree with size, treeSize
                 for (int i = 0; i < treeSize; i++){
@@ -43,6 +38,14 @@ public class Main {
                 t.printBST(BST.INORDER); 
                 t.printBST(BST.PREORDER);
                 t.printBST(BST.POSTORDER);
+                
+                //create variables
+                Node<Integer> root = t.getRoot();
+                int heightbst = t.height();
+                boolean perfect = t.isPerfect();
+                boolean complete = t.isComplete();
+                boolean doubles = t.hasDoubles();
+
                 System.out.println("Root: " + root.getValue()); //print root of tree
                 System.out.println("Height: " + heightbst); //print height of tree
                 System.out.println("Perfect: " + perfect); //print is tree perfect
@@ -71,20 +74,16 @@ public class Main {
         scanner.close();
 
         //var to store the level the value was found in
-        int i;
+        int i = t.level(value);
         //if the value is not found in the tree print msg
         if(!t.contains(value)) {
             System.out.println(value + " was not found in the tree");
         //if the value is stored in the root print msg
         } else if (t.parent(value) == null) {
-            //level is = 0 if value is found at root
-            i = t.level(value);
             //the root does not have a parent
             System.out.println(value + " has no parent because it is the root of the tree");
             System.out.println("The level of the value " + value + " is " + i);
         } else {
-            //update i to be the level the value was found in
-            i = t.level(value);
             //get parent of the value
             Node<Integer> p = t.parent(value);
             //print to terminal
